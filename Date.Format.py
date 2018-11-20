adb755fbe2b69ea779399497ee71c25628f8c059
choudharyusman <45047731+choudharyusman@users.noreply.github.com>
2018-11-21 01:07:04 +0500

    Update Date.Format.py

--- Date.Format.py
+++ Date.Format.py
@@ -1,6 +1,6 @@
-day= input('Enter Day:              ')
-hour= int(input('Enter Hour:       '))
-minute= int(input('Enter Minute:   '))
+day= input('Enter Day: ')
+hour= int(input('Enter Hour: '))
+minute= int(input('Enter Minute: '))
 seconds= int(input('Enter Seconds: '))
 print('{0},{1}:{2}:{3}'.format(day,hour,minute,seconds))
 
